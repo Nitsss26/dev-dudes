f85cf5e9a6d942aaeaa5eca311ae9d251c8a438c
Nitsss26 <99978200+Nitsss26@users.noreply.github.com>
2022-02-28 01:35:41 +0530

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,1 +1,44 @@
 
+pragma solidity ^0.4.24;
+contract Details {
+
+    string Area;
+    string MRP;
+    string About_Product;
+
+constructor() public {
+    Area = "Amritsar";
+    MRP  =   "MRP";
+}
+
+    function Get_Area () public view returns(string){
+
+        return Area;
+    }
+
+       function Set_Area (string _Area) public {
+
+        Area = _Area;
+    }
+
+    function Get_MRP() public view returns(string){
+
+        return MRP;
+    }
+
+       function Set_MRP(string _MRP) public {
+
+        MRP = _MRP;
+    }
+
+       function AboutProduct () public view returns(string){
+
+        return About_Product;
+    }
+
+       function Set_About (string _About_Product) public {
+
+        About_Product = _About_Product;
+    }
+
+}
